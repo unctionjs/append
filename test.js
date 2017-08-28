26182336989ba0d404641e4c840f0b7ef55e451c
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-27 18:50:59 -0700

    Going back to how other javascript packages do it, makes it eaiser on everyone :/

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 import {test} from "tap"
 
-import append from "./source"
+import append from "./"
 
 test(({same, end}) => {
   same(append("a")(["b"]), ["b", "a"])
