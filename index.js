605990bae4e682fa167ff09d995844c478efdcad
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:28 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 /* eslint-disable no-extra-parens, no-unused-expressions */
-
-import type from "@unction/type"
-
-export default function append (value: any): Function {
-  return function appendValue (orderedList: any): any {
+import type from "@unction/type";
+export default function append(value) {
+  return function appendValue(orderedList) {
     switch (type(orderedList)) {
-      case "String": {
-        return `${orderedList}${value}`
-      }
-      case "Array": {
-        return [...orderedList, value]
-      }
-      default: {
-        throw new Error(`append doesn't know how to deal with ${type(orderedList)}`)
-      }
+      case "String":
+        {
+          return `${orderedList}${value}`;
+        }
+
+      case "Array":
+        {
+          return [...orderedList, value];
+        }
+
+      default:
+        {
+          throw new Error(`append doesn't know how to deal with ${type(orderedList)}`);
+        }
     }
-  }
+  };
 }
